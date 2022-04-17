4e1f1575669e4011ff4f761558dd401f53199e16
maxim-savchuk <maxsavchuk1105@gmail.com>
2022-04-17 19:39:35 +0300

    fixing deploy

--- bin/server.js
+++ bin/server.js
@@ -4,7 +4,7 @@ const app = require('../app');
 const { DB_HOST, PORT = 3001 } = process.env;
 
 mongoose
-  .connect(DB_HOST)
+  .connect(DB_HOST.toString())
   .then(() =>
     app.listen(PORT, () => {
       console.log(`Server running. Use our API on port: ${PORT}`);
